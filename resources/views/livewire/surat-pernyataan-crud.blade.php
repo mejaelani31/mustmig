f466efd5b8b9c3dd344f7ea54d287df0656099c0
mejaelani31 <plnachmadjaelani@gmail.com>
2025-07-25 17:50:23 +0800

    Update

--- resources/views/livewire/surat-pernyataan-crud.blade.php
+++ resources/views/livewire/surat-pernyataan-crud.blade.php
@@ -114,9 +114,9 @@
                 <x-secondary-button wire:click="closeModal">
                     {{ __('Cancel') }}
                 </x-secondary-button>
-                <x-primary-button class="ms-2" wire:click="save" wire:loading.attr="disabled">
+                <x-button class="ms-2" wire:click="save" wire:loading.attr="disabled">
                     {{ __('Save') }}
-                </x-primary-button>
+                </x-button>
             </x-slot>
         </x-dialog-modal>
     </form>
